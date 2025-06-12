19c6524526b8e39ca010921646fbe53983d5c006
lucasolivo <thelucasolivo@gmail.com>
2025-06-12 11:10:00 -0700

    added makeCommands

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ Also added levels when catching a pokemon (between 1 and 10) and added a command
 
 Update 6/11: Added a 'party' command that lets the user see their current party of 6 and their leading pokemon. Will add a box and switch and switchlead command later.
 
-Update 6/12: Added a save and load feature along with a reset option, so that the Pokedex and party persist after the program is exited.
+Update 6/12: Added a save and load feature along with a reset option, so that the Pokedex and party persist after the program is exited. Also added a makeCommands function to clean up the repl.go file.
---
+++ makeCommands.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"github.com/lucasolivo/Pokedex/internal/pokecache"
+)
+
+func makeCommands(cfg *config, cache *pokecache.Cache) map[string]cliCommand {
+	commands := map[string]cliCommand{}
+
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback: func(cfg *config, args []string) error {
+			return commandHelp(commands)
+		},
+	}
+
+	commands["map"] = cliCommand{
+		name:        "map",
+		description: "Displays the next map of the Pokeworld",
+		callback: func(cfg *config, args []string) error {
+			return commandMap(cfg, cache)
+		},
+	}
+
+	commands["mapb"] = cliCommand{
+		name:        "mapb",
+		description: "Displays the previous map of the Pokeworld",
+		callback: func(cfg *config, args []string) error {
+			return commandMapb(cfg, cache)
+		},
+	}
+
+	commands["explore"] = cliCommand{
+		name:        "explore",
+		description: "Displays pokemon that can be found at a location",
+		callback: func(cfg *config, args []string) error {
+			return commandExplore(cfg, cache, args)
+		},
+	}
+
+	commands["catch"] = cliCommand{
+		name:        "catch",
+		description: "Throws a pokeball at a Pokemon",
+		callback: func(cfg *config, args []string) error {
+			return commandCatch(cfg, cache, args)
+		},
+	}
+
+	commands["ability"] = cliCommand{
+		name:        "ability",
+		description: "Generates a random ability (add number to get multiple)",
+		callback: func(cfg *config, args []string) error {
+			return commandAbility(cfg, args)
+		},
+	}
+
+	commands["inspect"] = cliCommand{
+		name:        "inspect",
+		description: "Displays info about a Pokemon in your Pokedex",
+		callback:    commandInspect,
+	}
+
+	commands["pokedex"] = cliCommand{
+		name:        "pokedex",
+		description: "Lists out all Pokémon in your Pokedex",
+		callback:    commandPokedex,
+	}
+
+	commands["candy"] = cliCommand{
+		name:        "candy",
+		description: "Gives one rare candy to a Pokemon of your choosing",
+		callback:    commandCandy,
+	}
+
+	commands["party"] = cliCommand{
+		name:        "party",
+		description: "Displays your current Pokemon party",
+		callback: func(cfg *config, args []string) error {
+			return commandParty(cfg)
+		},
+	}
+
+	commands["reset"] = cliCommand{
+		name:        "reset",
+		description: "Resets the Pokedex and party for a fresh start",
+		callback: func(cfg *config, args []string) error {
+			return commandReset(cfg)
+		},
+	}
+
+	return commands
+}
